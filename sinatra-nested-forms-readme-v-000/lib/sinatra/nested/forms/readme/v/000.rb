87cc6fda0aaf54a7d463e9c93cf2e949bd61c0a8
vickicello <vicki.cello@gmail.com>
2018-09-07 15:11:47 -0400

    created gemfile, bin and lib directories

---
+++ sinatra-nested-forms-readme-v-000/lib/sinatra/nested/forms/readme/v/000.rb
@@ -0,0 +1,15 @@
+require "sinatra/nested/forms/readme/v/000/version"
+
+module Sinatra
+  module Nested
+    module Forms
+      module Readme
+        module V
+          module 000
+            # Your code goes here...
+          end
+        end
+      end
+    end
+  end
+end
---
+++ sinatra-nested-forms-readme-v-000/lib/sinatra/nested/forms/readme/v/000/version.rb
@@ -0,0 +1,13 @@
+module Sinatra
+  module Nested
+    module Forms
+      module Readme
+        module V
+          module 000
+            VERSION = "0.1.0"
+          end
+        end
+      end
+    end
+  end
+end
